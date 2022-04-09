5f3c3679cda32c72eb578383888b2e1c930b7129
Lincon Class <linconclass@gmail.com>
2022-04-09 18:52:06 -0300

    commit inicial - sistemaVendas com DALe tela de login

---
+++ Sistema Vendas/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sistema_Vendas.Models;
+using System.Diagnostics;
+
+namespace Sistema_Vendas.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Menu()
+        {
+            return View();
+        }       
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel Login)
+        {
+            if (ModelState.IsValid)
+            {
+                bool loginOK = Login.ValidarLogin();               
+            }
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+      
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Sistema Vendas/Models/LoginModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using SistemaVendas.Uteis;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sistema_Vendas.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage="Informe o e-mail do usuário!")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage ="O e-mail informado é inválido!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Informe a senha do usuário!")]  
+        public string Senha { get; set; }
+
+
+        //Existe um problema grave de segurança com essa abordagem => SQL Inject
+        //Vamos depois criar um método mais adequado na próxima aula
+        public bool ValidarLogin()
+        {
+            string sql = $"SELECT ID FROM VENDEDOR WHERE EMAIL='{Email}' AND SENHA='{Senha}'";
+
+            DAL objDAL = new DAL();
+            DataTable dt = objDAL.RetDataTable(sql);
+            if(dt.Rows.Count == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
